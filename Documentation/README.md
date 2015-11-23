c1d167529da70928544da69aa18fbbefe306a6e0
Bastian <bastian.schwabe@redwhitesilver.com>
2015-11-23 23:34:23 +0100

    Add extension basic files

---
+++ Documentation/README.md
@@ -0,0 +1,4 @@
+Content Element Icons: Documentation
+======================
+
+Please visit https://github.com/neuedaten/content_element_icons for more informations and a preview list of the icons.
---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Content Element Icons',
+	'description' => 'An icon collection for custom content elements.',
+	'category' => 'backend',
+	'author' => 'Bastian Schwabe',
+	'author_email' => 'bas@neuedaten.de',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
